9be04ff10bfe01f64b0e0e27c9cf96f7f63bf601
unknown <trizahnabwire20@gmail.com>
2025-08-06 23:20:34 +0300

    Hero

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './searchBar.scss'
+import search_icon from '../../assets/search_icon.svg'
 
 const searchBar = () => {
+  const [query, setQuery] = React.useState({
+    category: 'Rent',
+    location: '',
+    minPrice: '',
+    maxPrice: ''
+  });
   return (
-    <div>searchBar</div>
+    <div className='searchBar'>
+      <div className="category">
+        <button>Rent</button>
+        <button>Airbnbs</button>
+      </div>
+      <form action="">
+        <input type="text" name="location" id="" placeholder='Location' />
+        <input type="number" name="minPrice" min={0} max={10000000} id="" placeholder='Min Price' />
+        <input type="text" name="maxPrice" min={0} max={10000000} id="" placeholder='Max Price' />
+        <button><img src={search_icon} alt="" /></button>
+      </form>
+    </div>
   )
 }
 
--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -11,21 +11,22 @@ const homePage = () => {
       </div>
       <SearchBar />
       <div className="stats">
+      <div className="stat">
         <h1>16+</h1>
         <h2>Years of Experience</h2>
       </div>
-      <div className="stats">
+      <div className="stat">
         <h1>200</h1>
         <h2>Awards Gained</h2>
       </div>
-      <div className="stats">
+      <div className="stat">
         <h1>450+</h1>
         <h2>Properties Ready</h2>
+      </div>
       </div>
       <div className="imgContainer">
           <img src={bg} alt="" />
       </div>
-
     </div>
   )
 }
