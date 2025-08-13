b11ccc0534eaf97451fe5308e12c951292daae11
unknown <trizahnabwire20@gmail.com>
2025-08-13 22:47:43 +0300

    Card

--- src/components/propertyCard/propertyCard.jsx
+++ src/components/propertyCard/propertyCard.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import './propertyCard.scss'
+import { Link } from 'react-router-dom'
+import location_icon from '../../assets/location_icon.svg'
+import bed_icon from '../../assets/bed_icon.svg'
+import bathroom_icon from '../../assets/bathroom_icon.svg'
+import save_icon from '../../assets/save_icon.svg'
+import chat_icon from '../../assets/chat_icon.svg'
+import share_icon from '../../assets/share_icon.svg'
 
-const propertyCard = () => {
+const PropertyCard = ({item}) => {
   return (
-    <div className='card'>propertyCard</div>
+    <div className='card'>
+      <Link to={`/${item.id}`} className="imageContainer">
+      <img src={item.image} alt="" />
+      </Link>
+      <div className="textContainer">
+        <h3 className="location">
+          <Link to={`/${item.id}`}>
+          <img src={location_icon} alt="" />
+          <span>{item.location}</span>
+          </Link>
+        </h3>
+
+        <p className="distance">{item.distance}</p>
+
+        <p className="price">
+          <span>Ksh {item.price}</span> / month
+        </p>
+        <div className="features">
+          <img src={bed_icon} alt="" />
+          <span className="bedrooms">{item.bedRooms} Bedrooms</span>
+          </div>
+          <div className="features">
+          <img src={bathroom_icon} alt="" />
+          <span className="bathrooms">{item.bathRooms} Bathrooms</span>
+          </div>
+
+          <div className="actions">
+            <div className="action">
+              <img src={save_icon} alt="" />
+            </div>
+            <div className="action">
+              <img src={chat_icon} alt="" />
+            </div>
+            <div className="action">
+              <img src={share_icon} alt="" />
+            </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
-export default propertyCard
+export default PropertyCard;
