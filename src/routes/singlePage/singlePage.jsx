44e22f46e12d62080b1a2da94bad1fe73b2aa0ac
unknown <trizahnabwire20@gmail.com>
2025-08-21 21:36:28 +0300

    single Property Info

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './singlePage.scss'
+import Slider from '../../components/slider/slider'
 
 const singlePage = () => {
   return (
-    <div>singlePage</div>
+    <div className='singlePage'>
+      <div className="details">
+        <div className="wrapper">
+          < Slider />
+          <div className="info">
+            <div className="top"></div>
+            <div className="bottom"></div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
   )
 }
 
