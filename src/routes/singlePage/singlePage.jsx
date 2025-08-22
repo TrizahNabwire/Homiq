425ae386e3b9902772118d15bbe59231adda5303
unknown <trizahnabwire20@gmail.com>
2025-08-22 23:41:05 +0300

    Details

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import './singlePage.scss'
 import Slider from '../../components/slider/slider'
+import { singlePropertyData, userData } from '../../lib/listData'
+import location_icon from '../../assets/location_icon.svg'
 
 const singlePage = () => {
   return (
     <div className='singlePage'>
       <div className="details">
         <div className="wrapper">
-          < Slider />
+          < Slider images={singlePropertyData.images}/>
           <div className="info">
-            <div className="top"></div>
+            <div className="top">
+              <div className="post">
+                <div className="location">
+                  <img src={location_icon} alt="" />
+                  <span>{singlePropertyData.location}</span>
+                </div>
+                <p className="distance">{singlePropertyData.distance}</p>
+              </div>
+              <div className="price">
+                <span>Ksh {singlePropertyData.price}</span> / month
+              </div>
+              <div className="user">
+                <img src={userData.image} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
             <div className="bottom"></div>
           </div>
         </div>
