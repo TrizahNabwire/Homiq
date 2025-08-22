cc7d0ee0b0a5462cb5c425319a0f09dc84de4623
unknown <trizahnabwire20@gmail.com>
2025-08-23 01:21:06 +0300

    Show Images

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './slider.scss'
+import { singlePropertyData } from '../../lib/listData'
 
-const slider = () => {
+const slider = ({images}) => {
   return (
-    <div>slider</div>
+    <div className='slider'>
+        <div className="bigImage">
+            <img src={images[0]} alt="" />
+        </div>
+        <div className="smallImages">
+            {images.slice(1).map((image, index) => (
+                // <div className="smallImage" >
+                    <img src={image} alt="" key={index}/>
+                // </div>
+            ))}
+        </div>
+
+    </div>
   )
 }
 
