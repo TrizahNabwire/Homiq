7406041a285737ca47bd999cdff30e518bc3aead
unknown <trizahnabwire20@gmail.com>
2025-08-26 22:07:25 +0300

    Slider

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './slider.scss'
 import { singlePropertyData } from '../../lib/listData'
+import left_icon from '../../assets/left_arrow.svg'
+import right_icon from '../../assets/right_arrow.svg'
 
 const slider = ({images}) => {
   return (
     <div className='slider'>
+        <div className="fullSlider">
+            <div className="arrow">
+                <img src={left_icon} alt="" />
+            </div>
+            <div className="imageContainer"></div>
+            <div className="arrow">
+                <img src={right_icon} alt="" />
+            </div>
+        </div>
         <div className="bigImage">
             <img src={images[0]} alt="" />
         </div>
