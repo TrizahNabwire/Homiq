f35e63208db201bcde3dd4da3993b1736a5b4e63
unknown <trizahnabwire20@gmail.com>
2025-08-28 23:24:33 +0300

    Slider styles

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './slider.scss'
 import { singlePropertyData } from '../../lib/listData'
 import left_icon from '../../assets/left_arrow.svg'
-import right_icon from '../../assets/right_arrow.svg'
+// import right_icon from '../../assets/right_arrow.svg'
 
 const slider = ({images}) => {
   return (
@@ -11,10 +11,13 @@ const slider = ({images}) => {
             <div className="arrow">
                 <img src={left_icon} alt="" />
             </div>
-            <div className="imageContainer"></div>
-            <div className="arrow">
-                <img src={right_icon} alt="" />
+            <div className="imageContainer">
+                <img src={images[0]} alt="" />
             </div>
+            <div className="arrow">
+                <img src={left_icon} className='rightIcon' alt="" />
+            </div>
+            <div className="closeIcon">x</div>
         </div>
         <div className="bigImage">
             <img src={images[0]} alt="" />
