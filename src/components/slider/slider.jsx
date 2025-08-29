76c122534090d62315e8a368b544613e529f440c
unknown <trizahnabwire20@gmail.com>
2025-08-29 22:58:54 +0300

    Slider Functionality

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -5,28 +5,38 @@ import left_icon from '../../assets/left_arrow.svg'
 // import right_icon from '../../assets/right_arrow.svg'
 
 const slider = ({images}) => {
+    const [imageIndex, setImageIndex] = React.useState(null);
+    const changeSlide = (direction) => {
+        if(direction === 'left'){
+            setImageIndex(imageIndex === 0 ? images.length - 1 : imageIndex - 1)
+        }
+        if(direction === 'right'){
+            setImageIndex(imageIndex === images.length - 1 ? 0 : imageIndex + 1)
+        }
+
+    }
   return (
     <div className='slider'>
+        {imageIndex !== null && (
         <div className="fullSlider">
-            <div className="arrow">
+            <div className="arrow" onClick={()=> changeSlide("left")}>
                 <img src={left_icon} alt="" />
             </div>
             <div className="imageContainer">
-                <img src={images[0]} alt="" />
+                <img src={images[imageIndex]} alt="" />
             </div>
-            <div className="arrow">
+            <div className="arrow" onClick={()=> changeSlide("right")}>
                 <img src={left_icon} className='rightIcon' alt="" />
             </div>
-            <div className="closeIcon">x</div>
+            <div className="closeIcon" onClick={()=> setImageIndex(null)}>x</div>
         </div>
+        )}
         <div className="bigImage">
-            <img src={images[0]} alt="" />
+            <img src={images[0]} alt="" onClick={()=>setImageIndex(0)}/>
         </div>
         <div className="smallImages">
             {images.slice(1).map((image, index) => (
-                // <div className="smallImage" >
-                    <img src={image} alt="" key={index}/>
-                // </div>
+                    <img src={image} alt="" key={index} onClick={()=>setImageIndex(index + 1)}/>
             ))}
         </div>
 
